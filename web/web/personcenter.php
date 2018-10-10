a1e4d8a3ff7187ad62bb624bce6cf419b8454bfa
Lucifer022 <38549021+Lucifer022@users.noreply.github.com>
2018-10-10 14:14:17 +0800

    Update personcenter.php

--- web/web/personcenter.php
+++ web/web/personcenter.php
@@ -19,12 +19,172 @@
     <title>个人中心</title>
     <link rel="stylesheet"  href="../css/index.css">
     <link rel="stylesheet"  href="../css/iconfont1.css">
+    <link rel="stylesheet" href="../css/weui.min.css">
     <link rel="stylesheet" href="https://res.wx.qq.com/open/libs/weui/1.1.2/weui.min.css"/>
     <script type="text/javascript" src="../js/jquery.min.js"></script>
     <script src="../js/iconfont.js"></script>
 </head>
-<body>
+<style>
+    .Mobcontent {
+        padding: 15px 30px;
+    }
 
+    .avatar {
+        width: 4rem;
+        height: 4rem;
+        border-radius: 50%;
+    }
+
+    .avatarandname {
+        display: inline-block;
+        /*line-height: 4rem;*/
+        align-items: center;
+        text-align: center;
+        list-style: none;
+    }
+
+    .nickname {
+        margin: 0 0 0 10px;
+    }
+
+    .lineheight {
+        line-height: 2rem;
+    }
+
+    .lineheight1 {
+        line-height: 2rem;
+        color: #fff;
+        padding: 0px 20px;
+        background: #202a36;
+        border-radius: 20px;
+        text-align: center;
+    }
+
+    .liststyle {
+        width: 100%;
+        padding: 10px 0;
+        float: left;
+    }
+
+    .iconfont {
+        font-size: 35px;
+        color: #fff;
+    }
+
+    .iconfont1 {
+        font-size: 35px !important;
+    }
+
+    .listitem {
+        width: calc(25% - 10px);
+        padding: 5px;
+        border-radius: 4px;
+        text-align: center;
+        display: inline-block;
+        float: left;
+        margin: 5px;
+        color: #000;
+    }
+
+    .iconstyle {
+        background: #6ecad5;
+        border-radius: 50%;
+        height: 60px;
+        width: 60px;
+        text-align: center;
+        line-height: 60px;
+        margin: 0 auto;
+    }
+
+    .listtext {
+        width: 100%;
+        font-size: 13px;
+        margin: 2px 0 0 0;
+    }
+
+    .weui_cell_ft:after{
+        height: 10px!important;
+        width: 10px!important;
+    }
+
+    p{
+        margin: 10px 0px;
+    }
+
+</style>
+<body ontouchstart="">
+<div class="Mobcontent" id="Mobcontent">
+    <div class="weui_cells_title" style="text-align: center">
+        <!--        带图标、说明、跳转的列表项-->
+        <ul class="avatarandname">
+            <li><img src="<?php echo $avatar; ?>" class="avatar"></li>
+            <li>
+                <div class="lineheight"><?php echo $nickname; ?></div>
+            </li>
+        </ul>
+    </div>
+    <div class="weui_cells weui_cells_access">
+        <a class="weui_cell" href="javascript:;">
+            <div class="weui_cell_hd">
+                <img src="../images/logo1.png" alt="icon" style="width:20px;margin-right:5px;display:block">
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>我的认证</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="javascript:;">
+            <div class="weui_cell_hd">
+                <img src="../images/logo1.png" alt="icon" style="width:20px;margin-right:5px;display:block">
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>我的发布</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="javascript:;">
+            <div class="weui_cell_hd">
+                <img src="../images/logo1.png" alt="icon" style="width:20px;margin-right:5px;display:block">
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>我的申请 </p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="javascript:;">
+            <div class="weui_cell_hd">
+                <img src="../images/logo1.png" alt="icon" style="width:20px;margin-right:5px;display:block">
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>我的手机</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="javascript:;">
+            <div class="weui_cell_hd">
+                <img src="../images/logo1.png" alt="icon" style="width:20px;margin-right:5px;display:block">
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>权限设置</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="../web/consoleweb/aboutus.php">
+            <div class="weui_cell_hd">
+                <img src="../images/logo1.png" alt="icon" style="width:20px;margin-right:5px;display:block">
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>关于我们</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+    </div>
 
 </body>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
