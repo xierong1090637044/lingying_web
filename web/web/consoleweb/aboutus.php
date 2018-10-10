59c1df492eae57ecf41ce38ee2ee6bf2dfd52d76
Lucifer022 <38549021+Lucifer022@users.noreply.github.com>
2018-10-10 14:15:37 +0800

    Create aboutus.php

---
+++ web/web/consoleweb/aboutus.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/10
+ * Time: 13:37
+ */
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <title>关于我们</title>
+    <link rel="stylesheet" href="../../css/bootstrap1.min.css">
+    <link rel="stylesheet" href="../../css/iconfont1.css">
+    <link rel="stylesheet" href="../../css/weui.min.css">
+    <script type="text/javascript" src="../../js/jquery.min.js"></script>
+    <script src="../../js/iconfont.js"></script>
+</head>
+<script>
+
+</script>
+<style>
+    .title {
+        background: #138496;
+        height: 20%;
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    .middle {
+        background: #6dbaa3;
+        height: 55%;
+        width: 100%;
+        overflow: hidden;
+    }
+
+    .middleinner {
+        width: 80%;
+        height: 40%;
+        border: 1px solid #ffffff;
+        margin: 10px auto;
+        text-align: center;
+        font-weight: bold;
+    }
+
+    .middlebottom {
+        width: 40%;
+        height: 40%;
+        margin: 0 auto;
+        border: solid 1px;
+    }
+
+    .footer {
+        background: #6dbaa3;
+        height: 25%;
+        width: 100%;
+        border-top: 2px white solid;
+        text-align: center;
+    }
+
+    .footercenter {
+        width: 60px;
+        height: 60px;
+        margin: 20px auto;
+        border: 1px solid #ffffff;
+        text-align: center;
+        font-weight: bold;
+        background: blue;
+        border-radius: 50%;
+    }
+</style>
+<body>
+<div class="title">
+    <a href="javascript:history.go(-1)" class="header-back jsBack" style="color: white"><img src="../../images/向左箭头.png" style="width: 4%">返回</a>
+</div>
+<div class="middle">
+    <div class="middleinner">
+        <p>那就顺其自然吧，反正你也无能为力<br/>
+            倒不如花时间让自己变得更好<br/>
+            然后用更好的自己去迎接更好的未来<br/>
+        </p>
+    </div>
+    <div class="middlebottom">
+    </div>
+</div>
+<div class="footer">
+    <div class="footercenter">
+        <a href="#">联 系<br/>我 们</a>
+    </div>
+    微信：<a href="#">xierong1090637044</a>或<a href="#">heqihang574631511</a><br/>
+    邮箱：<a href="#">xr@starytech.cn</a>
+</div>
+</body>
+</html>
